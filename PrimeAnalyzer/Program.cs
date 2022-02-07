c36c7cf3d4209010b0bc656afbcb673520b500ee
Dirk Krummacker <rasen_02cider@icloud.com>
2022-02-07 20:19:45 +0100

    Extract class for business logic

--- PrimeAnalyzer/Program.cs
+++ PrimeAnalyzer/Program.cs
@@ -1,19 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace PrimeAnalyzer
 {
     class Program
     {
-        /// <summary>a constant for a list of int with no elements</summary>
-        private static readonly List<int> EmptyList = new();
-
-        /// <summary>Called when the program is started.</summary>
-        /// <param name="args">Command line arguments. If specified, then
-        /// interpreted as a number that defines how many numbers are computed.
+        /// <summary>
+        /// Called when the program is started.
+        /// </summary>
+        /// <param name="args">
+        /// Command line arguments. If specified, then interpreted as a number
+        /// that defines how many numbers are computed.
         /// </param>
         private static void Main(string[] args)
         {
+            PrimeFactorDisassembler disassembler = new();
+
             int end = 1000;
             if (args.Length != 0)
             {
@@ -24,7 +25,7 @@ namespace PrimeAnalyzer
             {
                 Console.Write(i + " is");
 
-                var primeFactors = ComputePrimeFactors(i);
+                var primeFactors = disassembler.ComputePrimeFactors(i);
                 switch (primeFactors.Count)
                 {
                     case 0:
@@ -46,35 +47,6 @@ namespace PrimeAnalyzer
                 }
                 Console.WriteLine();
             }
-        }
-
-        /// <summary>For n>1, returns the list of prime numbers which,
-        /// multiplied with each other, result in n. For n<=1, returns the
-        /// empty list.</summary>
-        /// <param name="n">The number to split into prime factors.</param>
-        private static List<int> ComputePrimeFactors(int n)
-        {
-            if (n <= 1)
-            {
-                return EmptyList;
-            }
-
-            List<int> result = new();
-            for (int i = 2; i < n; ++i)
-            {
-                if (n % i == 0)
-                {
-                    result.Add(i);
-                    result.AddRange(ComputePrimeFactors(n / i));
-                    return result;
-                }
-            }
-
-            // if we iterated through the loop until this point then
-            // n is already a prime
-            result.Add(n);
-
-            return result;
         }
     }
 }
